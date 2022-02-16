96f9259c5fd9478ed4fe3ae1a5f27535f7804256
Titus Gahissy <titus@gahissy.com>
2022-02-16 17:31:41 +0000

    Add support prefix

--- samples/sample-default/build.gradle.kts
+++ samples/sample-default/build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
 
 dependencies {
     implementation(project(":soffa-foundation-service"))
-    implementation(project(":soffa-foundation-pubsub"))
+    implementation(project(":soffa-foundation-support-pubsub"))
     testImplementation(project(":soffa-foundation-test"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ include(":soffa-foundation-application")
 include(":soffa-foundation-models")
 include(":soffa-foundation-commons")
 include(":soffa-foundation-service")
-include(":soffa-foundation-pubsub")
+include(":soffa-foundation-support-pubsub")
 include(":soffa-foundation-test")
 
 include(":samples:sample-default")
