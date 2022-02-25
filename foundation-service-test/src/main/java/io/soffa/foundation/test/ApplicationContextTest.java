8325b18a8bf468da1e4fde7dbaac39f26eae5868
Titus Gahissy <titus@gahissy.com>
2022-02-25 02:43:38 +0000

    Release 1.9.11

--- foundation-service-test/src/main/java/io/soffa/foundation/test/ApplicationContextTest.java
+++ foundation-service-test/src/main/java/io/soffa/foundation/test/ApplicationContextTest.java
@@ -2,7 +2,6 @@ package io.soffa.foundation.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
